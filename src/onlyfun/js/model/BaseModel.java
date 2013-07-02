966a7f5e125ba01ebb322f7d1f0999fc228808ee
jishen521 <240896322@qq.com>
2013-07-02 16:54:05 +0800

    DSMS

---
+++ src/onlyfun/js/model/BaseModel.java
@@ -0,0 +1,76 @@
+package onlyfun.js.model;
+
+import java.util.Date;
+
+public class BaseModel {
+	private Long id;
+	private String discriminator;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getDiscriminator() {
+		return discriminator;
+	}
+	public void setDiscriminator(String discriminator) {
+		this.discriminator = discriminator;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public Boolean getSex() {
+		return sex;
+	}
+	public void setSex(Boolean sex) {
+		this.sex = sex;
+	}
+	public String getIdNo() {
+		return idNo;
+	}
+	public void setIdNo(String idNo) {
+		this.idNo = idNo;
+	}
+	public Date getBirthday() {
+		return birthday;
+	}
+	public void setBirthday(Date birthday) {
+		this.birthday = birthday;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getTel() {
+		return tel;
+	}
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+	private String name;
+	private String userId;
+	private String password;
+	private Boolean sex;
+	private String idNo;
+	private Date birthday;
+	private String email;
+	private String tel;
+}
